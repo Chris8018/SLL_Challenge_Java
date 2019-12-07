ff888741eda974835d718ee10bf3ed9cd2ddddd9
Chris <tranuytrieuvi@protonmail.com>
2019-12-07 19:38:42 +0700

    Day 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import PasswordValidator.PasswordValidator;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello world");
+//        System.out.println("Hello world");
+        challenge1();
+    }
+
+    public static void challenge1() {
+        /*
+        Sololearn -> F
+        John Doe -> F
+        $ololearn7 -> T
+         */
+
+        var testCase = new ArrayList<String>();
+        testCase.add("Sololearn");
+        testCase.add("John Doe");
+        testCase.add("$ololearn7");
+
+        for (String str : testCase) {
+            System.out.println("Case \"" + str + "\": " + PasswordValidator.validate(str));
+        }
     }
 }
---
+++ src/PasswordValidator/PasswordValidator.java
@@ -0,0 +1,43 @@
+package PasswordValidator;
+
+public class PasswordValidator {
+    /*
+    This is the 1st challenge I am going to do today
+
+    Validates passwords to mach specific rules.
+    Eg: the min length of password must be 8 chars long and it should have at least one uppercase letter in it.
+
+    RULE:
+    Min Length: 5
+    Max Length: 10;
+    Contains AT LEAST 1 number
+    Contains AT LEAST one special character
+    NOT contain space
+
+    ==> FAILED!! I check this for 37'
+    ==> Will complete this later
+     */
+
+    public static boolean validate(String password) {
+        return checkLength(password.length()) &&
+                noSpace(password) &&
+                containsNum(password) &&
+                containsSpecialChar(password);
+    }
+
+    private static boolean checkLength(int length) {
+        return length >= 5 && length <=10;
+    }
+
+    private static boolean noSpace(String str) {
+        return str.contains(" ");
+    }
+
+    private static boolean containsNum(String str) {
+        return str.matches(".*[0-9].*");
+    }
+
+    private static boolean containsSpecialChar(String str) {
+        return str.matches(".*[`~!@#$%^&*()\\\\-_=+\\\\\\\\|\\\\[{\\\\]};:'\\\",<.>/?].*");
+    }
+}
